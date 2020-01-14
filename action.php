be445c19f17687eb85919b4c7aa0925dff5b282b
zvodd <zv.odd.101@gmail.com>
2020-01-15 04:28:11 +0800

    fix syntax error, change labeling, remove unused method

--- action.php
+++ action.php
@@ -4,40 +4,29 @@ require_once( '../../php/Torrent.php' );
 require_once( "../../php/xmlrpc.php" );
 
 // Update conf.php with your parameters before running !
-
 eval( getPluginConf( 'webseedsource' ) );
 
 class WeebSeedTorrent extends Torrent {
-		protected $pointer = 0;
-	
-		public function __construct($torrent, $dlpath, $webseedurl) 
-		{
-			$torrentdata = get_object_vars($torrent);
-			foreach ($torrentdata as $key => $value){
-				$this->{$key} = $value;
-			}
-			$url = $webseedurl;
-			if ($dlpath != "") {
-				$url = $url . "/" . $dlpath . "/";
-			}
-			$this->{"url-list"} = [$url];
-			$this->{"announce-list"} = [];
-			$this->{"announce"} = null;
-			$this->{"private"} = 1;
-			return;
+	protected $pointer = 0;
+
+	public function __construct($torrent, $dlpath, $webseedurl)
+	{
+		$torrentdata = get_object_vars($torrent);
+		foreach ($torrentdata as $key => $value){
+			$this->{$key} = $value;
 		}
-	
-	public function getName($default) {
-		if(is_null($this->name)){
-			return $default;
+		$url = $webseedurl;
+		if ($dlpath != "") {
+			$url = $url . "/" . $dlpath . "/";
 		}
-		else{
-			return $this->name;	
-		}
+		$this->{"url-list"} = [$url];
+		$this->{"announce-list"} = [];
+		$this->{"announce"} = null;
+		$this->{"private"} = 1;
+		return;
 	}
+
 }
-
-
 function serve_file($webseedurl, $webseedbase){
 	if(isset($_REQUEST['result']))
 		cachedEcho('noty(theUILang.cantFindTorrent,"error");',"text/html");
@@ -57,21 +46,21 @@ function serve_file($webseedurl, $webseedbase){
 			$basepath = trim(str_replace($filename, "", $basepath), "/");
 			$newtorrent = new WeebSeedTorrent($torrent, $basepath, $webseedurl);
 			toLog("Generating torrent with url_list:" . implode(",", $newtorrent->{"url-list"}));
-			
+
 			if (is_null($filename)){
-				$filename = 'webseed_'.$newtorrent->getName($_REQUEST['hash']).'.torrent';
+				$filename = '(webseed) '.$newtorrent->getName($_REQUEST['hash']).'.torrent';
 			}else{
-                $filename = 'webseed_'.$filename
+                $filename = '(webseed) '.$filename.'.torrent';
             }
 			if(isset($_SERVER['HTTP_USER_AGENT']) && strstr($_SERVER['HTTP_USER_AGENT'],'MSIE')){
 				$filename = rawurlencode($filename);
 			}
+
 			if ($newtorrent)
-				$newtorrent->send();
+				$newtorrent->send($filename);
 		}
 	}
 }
-
 serve_file($webseedurl, $webseedbase);
 header("HTTP/1.0 302 Moved Temporarily");
 header("Location: ".$_SERVER['PHP_SELF'].'?result=0');
